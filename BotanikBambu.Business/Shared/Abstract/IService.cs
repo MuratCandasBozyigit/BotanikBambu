e5949e43f27939c5b24a27be1414125b05555454
MURAT CANDAŞ BOZYİĞİT <bozyigit31@gmail.com>
2024-08-15 13:43:54 +0300

    Business ServiceRepo

---
+++ BotanikBambu.Business/Shared/Abstract/IService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotanikBambu.Business.Shared.Abstract
+{
+    public interface IService
+    {
+    }
+}
