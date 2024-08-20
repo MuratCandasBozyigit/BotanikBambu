49c6108236f9a957e80764afec9045b0307bb32d
MURAT CANDAŞ BOZYİĞİT <bozyigit31@gmail.com>
2024-08-20 08:52:35 +0300

    Bambu Index ve modal modeal yerine tema kullan dha düzgün gzel gözüken bir ekleme sayfası olsun hatta bu bambu ekleme sayfası için ındex dışında bir add sınıfı oluştur içerisine ajax ile silme oluştur daa mantıklı kodları ufaktan kopyalayıp değişirsin ? ...   Shop kısmına tema bul indir giydirmeye calış iyi kötü.

---
+++ BotanikBambu.Web/BotanikBambu.Web/Areas/Shop/Controllers/FavouritesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BotanikBambu.Web.Areas.Shop.Controllers
+{
+    public class FavouritesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
